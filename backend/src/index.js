bf0b661526ffdbe2a8c7412f0fa2513a03080fa0
Adarsh Shukla <150530437+shuklaAdarsh2608@users.noreply.github.com>
2025-08-16 01:49:45 +0530

    Message contenet

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
@@ -16,7 +15,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+// ✅ Increase payload limit to handle image base64
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
